d405542619335f1d455d936f18d7073c05ac288b
Veljko Blagojevic <veljko@geta.no>
2020-10-10 09:49:46 +0200

    Serving & Fetching Data from Express

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 import { Link } from "react-router-dom";
 import {
@@ -11,11 +12,20 @@ import {
   Button,
 } from "react-bootstrap";
 import Rating from "components/Rating";
-import products from "../products";
 
 const ProductScreen = ({ match }) => {
-  const product = products.find((p) => p._id === match.params.id);
+  const [product, setProduct] = useState({});
+
+  useEffect(() => {
+    async function fetchProduct() {
+      const { data } = await axios.get(`/api/products/${match.params.id}`);
+      setProduct(data);
+    }
+    fetchProduct();
+  }, [match.params.id]);
+
   const isProductInStock = !!product.countInStock;
+
   return (
     <>
       <Link className="btn btn-light my-3 " to="/">
